7041bfb4779453e020f1aeae3341d1effdda73c7
jannecook <135638400+jannecook@users.noreply.github.com>
2023-09-21 17:46:18 -0700

    Require at least one character class to be chosen

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,10 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var lower; 
+var upper; 
+var number; 
+var special;
 
 //Enable button
 document.getElementById("generate").disabled = false;
@@ -20,12 +24,22 @@ generateBtn.addEventListener("click", function passwordParameters() {
     var uppercaseConfirm = window.confirm("Select 'Okay' if you would like to include uppercase letters in your password. Select 'Cancel' if you do not wish to include uppercase letters. Note: The more character types included, the more secure your password will be.");
     var numberConfirm = window.confirm("Select 'Okay' if you would like to include numbers in your password. Select 'Cancel' if you do not wish to include numbers. Note: The more character types included, the more secure your password will be.");
     var specialCharConfirm = window.confirm("Select 'Okay' if you would like to include special characters (e.g. #, $, &) in your password. Select 'Cancel' if you do not wish to include special characters. Note: The more character types included, the more secure your password will be.");
+    var lower = lowercaseConfirm ? 1 : 0;
+    var upper = uppercaseConfirm ? 1 : 0;
+    var number = numberConfirm ? 1 : 0;
+    var special = specialCharConfirm ? 1 : 0;
+    if (lower + upper + number + special == 0) {
+      alert("Please select 'Okay' for at least one character type.")
+    }
+    else {
+      console.log(lowercaseConfirm, uppercaseConfirm, numberConfirm, specialCharConfirm)
+    }
   } else {
     alert("Please select a number larger than 7 and smaller than 129.");
   }
 })
 var responses = "";
-
+/*
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -37,3 +51,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
